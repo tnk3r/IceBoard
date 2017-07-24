1d60230b8e72baefb2503ae278ba904bbf0d9e3d
tnk3r <abcfilm@gmail.com>
2017-07-24 09:37:56 -0700

    V175 commit, July 24 2017

--- ArdNano/IceBoard_ver_4/IceBoard_ver_4.ino
+++ ArdNano/IceBoard_ver_4/IceBoard_ver_4.ino
@@ -1,97 +1,125 @@
 #include <EEPROM.h>
-
-int channel1 = 250;
-int channel2 = 250;
-int channel3 = 250;
-int channel4 = 250;
-int channel5 = 250;
-int channel6 = 250;
 // temp3, temp2, temp1 = A5, A6, A7
 
 void setup()  {
+  
   Serial.begin(9600);
-  Serial.println("IceBoard_rv2_v160");
+  Serial.println("IceBoard_rv2_v175");
   Serial.println("Made by .tink3r!");
-  Serial.println("Mmhwaa-ahaha!");
-  Serial.println("send me a 4 digit number,\n Example 4255 to set Channel 4 at 100%");
-  Serial.println("");
-  Serial.println(1000 + EEPROM.read(0));
-  Serial.println(2000 + EEPROM.read(1));
-  Serial.println(3000 + EEPROM.read(2));
-  Serial.println(4000 + EEPROM.read(3));
-  Serial.println(5000 + EEPROM.read(4));
-  Serial.println(6000 + EEPROM.read(5));
-
+  printEEPROM();
+  printTemp();
+  
 }
 
 void loop()  {
-
+  
   if (Serial.available() > 0) {
     String message = Serial.readString();
     int speed1 = message.substring(1).toInt();
     int fan = message.substring(0, 1).toInt();
-    Serial.println(message);
+    if (message.substring(0, 3) == "MAX") {
+      setAllFanValue(255);
+      delay(50);
+      printEEPROM();
+    } else {
+      if (message.substring(0,3) == "MIN"){
+          setAllFanValue(10);
+          delay(50);
+          printEEPROM();
+      } else {
+        setFanSpeed(fan, speed1);
+        Serial.println(message);
+        delay(10);
+      } 
+    }
+  } else {
+    setPWMFromEEPROM();
+    if (EEPROM.read(7) == 250) {
+        printEEPROM();
+        printTemp();
+        EEPROM.write(7, 0);
+    } else {
+    int val = EEPROM.read(7) + 1;
+    EEPROM.write(7, val);
+    delay(100);
+    }
+  }
+}
 
+void setPWMFromEEPROM(){
+  
+    analogWrite(3, EEPROM.read(0));
+    analogWrite(5, EEPROM.read(1));
+    analogWrite(6, EEPROM.read(2));
+    analogWrite(9, EEPROM.read(3));
+    analogWrite(10, EEPROM.read(4));
+    analogWrite(11, EEPROM.read(5));
+}
+void printEEPROM(){
+  
+    Serial.println(1000 + EEPROM.read(0));
+    Serial.println(2000 + EEPROM.read(1));
+    Serial.println(3000 + EEPROM.read(2));
+    Serial.println(4000 + EEPROM.read(3));
+    Serial.println(5000 + EEPROM.read(4));
+    Serial.println(6000 + EEPROM.read(5));
+}
+
+void setFanSpeed(int fan, int speed1){
+  
     if (fan == 1) {
-      channel1 = speed1;
-      EEPROM.write(0, channel1);
-      analogWrite(3, channel1);
+      EEPROM.write(0, speed1);
+      analogWrite(3, speed1);
     }
     if (fan == 2) {
-      channel2 = speed1;
-      EEPROM.write(1, channel2);
-      analogWrite(5, channel2);
+      EEPROM.write(1, speed1);
+      analogWrite(5, speed1);
     }
     if (fan == 3) {
-      channel3 = speed1;
-      EEPROM.write(2, channel3);
-      analogWrite(6, channel3);
+      EEPROM.write(2, speed1);
+      analogWrite(6, speed1);
     }
     if (fan == 4) {
-      channel4 = speed1;
-      EEPROM.write(3, channel4);
-      analogWrite(9, channel4);
+      EEPROM.write(3, speed1);
+      analogWrite(9, speed1);
     }
     if (fan == 5) {
-      channel5 = speed1;
-      EEPROM.write(4, channel5);
-      analogWrite(10, channel5);
+      EEPROM.write(4, speed1);
+      analogWrite(10, speed1);
     }
     if (fan == 6) {
-      channel6 = speed1;
-      EEPROM.write(5, channel6);
-      analogWrite(11, channel6);
+      EEPROM.write(5, speed1);
+      analogWrite(11, speed1);
     }
-    Serial.println(message);
-    delay(10);
-  } else {
+}
 
-    channel1 = EEPROM.read(0);
-    channel2 = EEPROM.read(1);
-    channel3 = EEPROM.read(2);
-    channel4 = EEPROM.read(3);
-    channel5 = EEPROM.read(4);
-    channel6 = EEPROM.read(5);
+void setAllFanValue(int value){
+  
+      analogWrite(3, value);
+      analogWrite(5, value);
+      analogWrite(6, value);
+      analogWrite(9, value);
+      analogWrite(10, value);
+      analogWrite(11, value);
+      EEPROM.write(0, value);
+      EEPROM.write(1, value);
+      EEPROM.write(2, value);
+      EEPROM.write(3, value);
+      EEPROM.write(4, value);
+      EEPROM.write(5, value);
+}
 
-    analogWrite(3, channel1);
-    analogWrite(5, channel2);
-    analogWrite(6, channel3);
-    analogWrite(9, channel4);
-    analogWrite(10, channel5);
-    analogWrite(11, channel6);
-    
-    int t1 = analogRead(7);
-    int t2 = analogRead(6);
-    int t3 = analogRead(5);
-    Serial.print("TMP:");
-    Serial.print(t1);
-    Serial.print(":");
-    Serial.print(t2);
-    Serial.print(":");
-    Serial.print(t3);
-    Serial.println(":");
-    // delay(1000);
-    delay(100);
-  }
+void printTemp(){
+  
+      int t1 = analogRead(7);
+      int t2 = analogRead(6);
+      int t3 = analogRead(5);
+      Serial.print("TMP:");
+      Serial.print(t1);
+      Serial.print(":");
+      Serial.print(t2);
+      Serial.print(":");
+      Serial.print(t3);
+      Serial.println(":");
 }
 
